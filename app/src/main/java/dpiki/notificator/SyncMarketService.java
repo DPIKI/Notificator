25bcb184493bda570cd843dc82e82f0ea0d303a7
Vlad <Vlad81611@yandex.ru>
2016-07-05 12:01:23 +0300

    Some refactoring

--- app/src/main/java/dpiki/notificator/SyncMarketService.java
+++ app/src/main/java/dpiki/notificator/SyncMarketService.java
@@ -5,10 +5,11 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.Context;
-import android.support.v7.app.NotificationCompat;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.RequestFuture;
@@ -27,6 +28,8 @@ import dpiki.notificator.data.Recommendation;
 
 public class SyncMarketService extends IntentService {
     public static final String TAG = "SyncMarket";
+    public static final String PREF_KEY_NOTIFY_ID = "notifyId";
+    public static final String PREF_KEY_LAST_DATE = "lastDate";
 
     RequestQueue queue;
 
@@ -37,7 +40,6 @@ public class SyncMarketService extends IntentService {
     @Override
     public void onCreate() {
         super.onCreate();
-
         queue = Volley.newRequestQueue(this);
     }
 
@@ -48,7 +50,11 @@ public class SyncMarketService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+
+        String lastDate = pref.getString(PREF_KEY_LAST_DATE, "");
+        if (!lastDate.equals("")) {
             RequestFuture<JSONObject> future = RequestFuture.newFuture();
             JsonObjectRequest request = new JsonObjectRequest(
                     JsonObjectRequest.Method.GET,
@@ -60,34 +66,13 @@ public class SyncMarketService extends IntentService {
                 ArrayList<Phone> phones = extractResponse(response);
                 ArrayList<MarketClient> clients = readClients();
                 ArrayList<Recommendation> recommendations = filterNewPhones(phones, clients);
-
-                NotificationManager nm =
-                        (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-                for (Recommendation i : recommendations) {
-                    NotificationCompat.Builder builder =
-                            new NotificationCompat.Builder(context)
-                            .setSmallIcon(android.R.drawable.ic_dialog_alert)
-                            .setWhen(System.currentTimeMillis())
-                            .setDefaults(Notification.DEFAULT_ALL)
-                            .setContentTitle("Есть рекомендация")
-                            .setContentText("Клиент: " + i.client.getName() +
-                                            "\nТелефон: " + i.phone.getName());
-                    Notification n = builder.build();
-                    nm.notify(notifId, n);
-                    notifId++;
-                    editor.putInt(KEY_NOTIF_ID, notifId);
-                    editor.apply();
-                }
-
+                notifyUser(recommendations);
                 Log.d(TAG, response.toString());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (TimeoutException e) {
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
                 e.printStackTrace();
             }
+        } else {
+
         }
     }
 
@@ -102,5 +87,30 @@ public class SyncMarketService extends IntentService {
     ArrayList<Recommendation> filterNewPhones(ArrayList<Phone> phones,
                                               ArrayList<MarketClient> filter) {
         return new ArrayList<>();
+    }
+
+    void notifyUser(ArrayList<Recommendation> recommendations) {
+        NotificationManager nm =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+        Integer notifyId = pref.getInt(PREF_KEY_NOTIFY_ID, 0);
+
+        for (Recommendation i : recommendations) {
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                            .setWhen(System.currentTimeMillis())
+                            .setDefaults(Notification.DEFAULT_ALL)
+                            .setContentTitle("Есть рекомендация")
+                            .setContentText("Клиент: " + i.client.getName() +
+                                    "\nТелефон: " + i.phone.getName());
+            Notification n = builder.build();
+            nm.notify(notifyId, n);
+            notifyId++;
+            editor.putInt(PREF_KEY_NOTIFY_ID, notifyId);
+            editor.apply();
+        }
     }
 }
