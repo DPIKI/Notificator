53dc8f5bfc8ed3192f99aa6e8f724f37c5bee58e
konstantinow <prog.13@outlook.com>
2016-07-07 11:24:46 +0300

    Some tests

--- app/src/test/java/dpiki/notificator/SyncMarketServiceTest.java
+++ app/src/test/java/dpiki/notificator/SyncMarketServiceTest.java
@@ -29,10 +29,13 @@ import dpiki.notificator.data.Phone;
 @Config(constants = BuildConfig.class, sdk = 18)
 public class SyncMarketServiceTest {
     private SyncMarketService marketService;
+    private SimpleDateFormat sdf;
 
     @Before
     public void setUp() throws Exception {
         marketService = Robolectric.buildService(SyncMarketService.class).get();
+        sdf = new SimpleDateFormat();
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss");
     }
 
     @Test
@@ -41,62 +44,72 @@ public class SyncMarketServiceTest {
     }
 
     @Test
-    public void testExtractResponse() throws Exception {
+    public void extractResponse_isCorrect() throws Exception {
         JSONObject jsonObject;
         ArrayList<Phone> phones;
         JSONArray jsonArray;
-        JSONObject phoneJSON;
 
+        //------------------------test_with_success==false------------------------------------------
         jsonObject = new JSONObject();
         jsonObject.put(marketService.JSON_KEY_SUCCESS, false);
         phones = marketService.extractResponse(jsonObject);
-        Assert.assertEquals("Field success == false -> " +
-                "returned size!= 0",
-                0, phones.size());
-
-
-        String sDate = "2016-01-01 10:00:00";
-        SimpleDateFormat sdf = new SimpleDateFormat();
-        sdf.applyPattern("yyyy-MM-dd HH:mm:ss");
-        Date date = sdf.parse(sDate);
-        Phone phone =
-                new Phone(
-                        1,
-                        "Nokia",
-                        "param1",
-                        "param2",
-                        "param3",
-                        date);
-        phones.add(phone);
+        Assert.assertEquals("Field success == false -> returned size!= 0", 0, phones.size());
+        //------------------------test_with_JSONArray-----------------------------------------------
+        jsonArray = new JSONArray();
         jsonObject = new JSONObject();
         jsonObject.put(marketService.JSON_KEY_SUCCESS, true);
-        phoneJSON = new JSONObject();
-
-        phoneJSON.put(marketService.JSON_KEY_ID, phone.getId());
-        phoneJSON.put(marketService.JSON_KEY_NAME, phone.getName());
-        phoneJSON.put(marketService.JSON_KEY_PARAM1, phone.getParam1());
-        phoneJSON.put(marketService.JSON_KEY_PARAM2, phone.getParam2());
-        phoneJSON.put(marketService.JSON_KEY_PARAM3, phone.getParam3());
-        phoneJSON.put(marketService.JSON_KEY_DATE, sDate);
-        jsonArray = new JSONArray();
-        jsonArray.put(phoneJSON);
+        //------------------------phone1------------------------------------------------------------
+        String sDate = "2016-01-01 10:00:00";
+        Phone phone = new Phone( 1, "Nokia", "param1", "param2", "param3", sdf.parse(sDate));
+        phones.add(phone);
+        jsonArray.put(getJSONFromPhone(phone));
+        //------------------------phone2------------------------------------------------------------
+        sDate = "2016-12-12 6:00:00";
+        phone = new Phone( 2, "Samsung", "p1", "p2", "p3", sdf.parse(sDate));
+        phones.add(phone);
+        jsonArray.put(getJSONFromPhone(phone));
+        //------------------------------------------------------------------------------------------
         jsonObject.put(marketService.JSON_KEY_PHONES, jsonArray);
-        ArrayList<Phone> arrayList = marketService.extractResponse(jsonObject);
+        ArrayList<Phone> retList = marketService.extractResponse(jsonObject);
 
-        boolean isEqual = false;
+        Assert.assertEquals(
+                "(Exp)Size:" + phones.size() + " != (Act)Size: " + retList.size(),
+                phones.size(), retList.size());
+
         for (int i = 0; i < phones.size(); i++) {
-            if (phones.get(i).getId() == arrayList.get(i).getId())
-                if(phones.get(i).getName().equals(arrayList.get(i).getName()))
-                    if(phones.get(i).getParam1().equals(arrayList.get(i).getParam1()))
-                        if(phones.get(i).getParam2().equals(arrayList.get(i).getParam2()))
-                            if(phones.get(i).getParam3().equals(arrayList.get(i).getParam3()))
-                                if(phones.get(i).getDate().toString().equals(
-                                        arrayList.get(i).getDate().toString())) {
-                                    isEqual = true;
-                                }
-            Assert.assertEquals("phone(id = " + phones.get(i).getId() + ")" +
-                    " != jsonPhone", true, isEqual);
-            isEqual = false;
+            Phone expected = phones.get(i);
+            Phone actual = retList.get(i);
+            Assert.assertEquals(
+                    "(Exp)Id: " + expected.getId() + "!= (Act)Id: " + actual.getId()
+                    ,expected.getId(), actual.getId());
+            Assert.assertEquals(
+                    "(Exp)Name: " + expected.getName() + "!= (Act)Name: " + actual.getName()
+                    ,expected.getName(), actual.getName());
+            Assert.assertEquals(
+                    "(Exp)Param1: " + expected.getParam1() + "!= (Act)Param1: " + actual.getParam1()
+                    ,expected.getParam1(), actual.getParam1());
+            Assert.assertEquals(
+                    "(Exp)Param2: " + expected.getParam2() + "!= (Act)Param2: " + actual.getParam2()
+                    ,expected.getParam2(), actual.getParam2());
+            Assert.assertEquals(
+                    "(Exp)Param3: " + expected.getParam3() + "!= (Act)Param3: " + actual.getParam3()
+                    ,expected.getParam3(), actual.getParam3());
+            Assert.assertEquals(
+                    "(Exp)Date: " + expected.getDate() + "!= (Act)Date: " + actual.getDate()
+                    ,expected.getDate(), actual.getDate());
         }
+    }
+
+    public JSONObject getJSONFromPhone(Phone phone) throws Exception{
+        JSONObject phoneJSON = new JSONObject();
+
+        phoneJSON.put(SyncMarketService.JSON_KEY_ID, phone.getId());
+        phoneJSON.put(SyncMarketService.JSON_KEY_NAME, phone.getName());
+        phoneJSON.put(SyncMarketService.JSON_KEY_PARAM1, phone.getParam1());
+        phoneJSON.put(SyncMarketService.JSON_KEY_PARAM2, phone.getParam2());
+        phoneJSON.put(SyncMarketService.JSON_KEY_PARAM3, phone.getParam3());
+        phoneJSON.put(SyncMarketService.JSON_KEY_DATE, sdf.format(phone.getDate()));
+
+        return phoneJSON;
     }
 }
