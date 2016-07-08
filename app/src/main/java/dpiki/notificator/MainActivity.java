644d5a5641d7c7132b8262c6ad0de59c970da921
konstantinow <prog.13@outlook.com>
2016-07-08 11:02:24 +0300

    Added Recycler adapter

--- app/src/main/java/dpiki/notificator/MainActivity.java
+++ app/src/main/java/dpiki/notificator/MainActivity.java
@@ -2,6 +2,8 @@ package dpiki.notificator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -13,6 +15,9 @@ import dpiki.notificator.network.BootReceiver;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager rvLayoutManager;
+    private RecyclerAdapter recyclerAdapter;
 
     EditText editText;
 
@@ -22,10 +27,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = (EditText)findViewById(R.id.et_ip_address);
+        recyclerView = (RecyclerView) findViewById(R.id.activity_main_recycler_view) ;
+        recyclerView.setHasFixedSize(true);
 
         BootReceiver.initAlarmManager(this);
 
         ArrayList<MarketClient> clients = DatabaseHelper.readClients(this);
+        recyclerAdapter = new RecyclerAdapter(clients);
+
+        recyclerView.setAdapter(recyclerAdapter);
+
         for (MarketClient client : clients) {
             Log.d(TAG,
                     client.getId().toString() + " " +
@@ -35,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                     client.getPref3()
             );
         }
+
     }
 
     public void onChangeIpClick(View v) {
