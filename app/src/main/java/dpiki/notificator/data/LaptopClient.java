6ef06257062115cba4230285440997896eceb0f3
Vlad <Vlad81611@yandex.ru>
2016-07-26 13:34:35 +0300

    bufv2.0

--- app/src/main/java/dpiki/notificator/data/LaptopClient.java
+++ app/src/main/java/dpiki/notificator/data/LaptopClient.java
@@ -3,7 +3,7 @@ package dpiki.notificator.data;
 /**
  * Created by Lenovo on 25.07.2016.
  */
-public class ClientLaptops extends Client {
+public class LaptopClient extends Client {
     public String pref11;
     public String pref12;
     public String pref13;
