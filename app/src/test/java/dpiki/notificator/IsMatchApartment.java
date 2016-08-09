0bab387ce20e5987cfa5393280cb547cf8377775
Vlad <Vlad81611@yandex.ru>
2016-08-09 23:07:39 +0300

    Almost all cases in Appartment.isMatch were tested

---
+++ app/src/test/java/dpiki/notificator/IsMatchApartment.java
@@ -0,0 +1,135 @@
+package dpiki.notificator;
+
+import static org.junit.Assert.*;
+import static dpiki.notificator.TestUtils.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import dpiki.notificator.network.dataobjects.Apartment;
+import dpiki.notificator.network.dataobjects.ApartmentReq;
+
+/**
+ * Created by User on 09.08.2016.
+ */
+public class IsMatchApartment {
+    Apartment a;
+    ApartmentReq ar;
+
+    @Before
+    public void before() {
+        a = new Apartment();
+        a.id = 0L;
+        a.firm = 1;
+        a.cost = 100.0;
+        a.floor = 1;
+        a.floorAll = 4;
+        a.kitchenArea = 10.0;
+        a.livingArea = 20.0;
+        a.totalArea = 30.0;
+        a.idFund = 1L;
+        a.idState = 1L;
+        a.idTypeApartment = 1L;
+        a.idWallMaterial = 1L;
+        a.idAddress = 1L;
+
+        ar = new ApartmentReq();
+        ar.costFrom = 50.0;
+        ar.costTo = 150.0;
+        ar.firm = 1;
+        ar.floor = 1;
+        ar.floorAll = 4;
+        ar.idAddress = 1L;
+        ar.idTypeApartment = 1L;
+        ar.idState = 1L;
+        ar.idWallMaterial = 1L;
+        ar.idFund = 1L;
+        ar.idRequirements = 1L;
+        ar.livingAreaFrom = 15.0;
+        ar.livingAreaTo = 25.0;
+        ar.kitchenAreaFrom = 5.0;
+        ar.kitchenAreaTo = 15.0;
+        ar.totalAreaFrom = 25.0;
+        ar.totalAreaTo = 35.0;
+        ar.notFirst = 0;
+        ar.notLast = 0;
+    }
+
+    @Test
+    public void testSuccessMatch() {
+        assertEquals(true, a.isMatch(ar));
+    }
+
+    @Test
+    public void testFirm() {
+        ar.firm = 0;
+        assertEquals(false, a.isMatch(ar));
+        a.firm = 0;
+        assertEquals(true, a.isMatch(ar));
+        ar.firm = 1;
+        assertEquals(false, a.isMatch(ar));
+        a.firm = 1;
+        assertEquals(true, a.isMatch(ar));
+        ar.firm = null;
+        assertEquals(true, a.isMatch(ar));
+        a.firm = 0;
+        assertEquals(true, a.isMatch(ar));
+        a.firm = null;
+        assertEquals(true, a.isMatch(ar));
+        ar.firm = 1;
+        assertEquals(false, a.isMatch(ar));
+
+        ar.firm = 1;
+        a.firm = 1;
+    }
+
+    @Test
+    public void testCost() throws NoSuchFieldException, IllegalAccessException {
+        testRange(a, ar, "cost", "costFrom", "costTo");
+    }
+
+    @Test
+    public void testKitchenArea() throws NoSuchFieldException, IllegalAccessException {
+        testRange(a, ar, "kitchenArea", "kitchenAreaFrom", "kitchenAreaTo");
+    }
+
+    @Test
+    public void testLivingArea() throws NoSuchFieldException, IllegalAccessException {
+        testRange(a, ar, "livingArea", "livingAreaFrom", "livingAreaTo");
+    }
+
+    @Test
+    public void testTotalArea() throws NoSuchFieldException, IllegalAccessException {
+        testRange(a, ar, "totalArea", "totalAreaFrom", "totalAreaTo");
+    }
+
+    @Test
+    public void testIdFund() throws NoSuchFieldException, IllegalAccessException {
+        testId(a, ar, "idFund", "idFund");
+    }
+
+    @Test
+    public void testIdTypeApartment() throws NoSuchFieldException, IllegalAccessException {
+        testId(a, ar, "idTypeApartment", "idTypeApartment");
+    }
+
+    @Test
+    public void testIdState() throws NoSuchFieldException, IllegalAccessException {
+        testId(a, ar, "idState", "idState");
+    }
+
+    @Test
+    public void testIdWallMaterial() throws NoSuchFieldException, IllegalAccessException {
+        testId(a, ar, "idWallMaterial", "idWallMaterial");
+    }
+
+    @Test
+    public void testIdAddress() throws NoSuchFieldException, IllegalAccessException {
+        testId(a, ar, "idAddress", "idAddress");
+    }
+
+    @Test
+    public void testFloor() {
+        // TODO : implement
+    }
+
+}
---
+++ app/src/test/java/dpiki/notificator/TestUtils.java
@@ -0,0 +1,99 @@
+package dpiki.notificator;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
+
+import dpiki.notificator.network.dataobjects.RealtyBase;
+import dpiki.notificator.network.dataobjects.RequirementBase;
+
+/**
+ * Created by User on 09.08.2016.
+ */
+public class TestUtils {
+
+    public static void testRange(RealtyBase a, RequirementBase ar, String fieldName, String limit1name, String limit2name) throws NoSuchFieldException, IllegalAccessException {
+        Field fldTest = a.getClass().getField(fieldName);
+        Field fldLimit1 = ar.getClass().getField(limit1name);
+        Field fldLimit2 = ar.getClass().getField(limit2name);
+
+        Double oldTest = (Double) fldTest.get(a);
+        Double oldLimit1 = (Double) fldLimit1.get(ar);
+        Double oldLimit2 = (Double) fldLimit2.get(ar);
+
+         /** when parameter is not specified and at least one limit is specified return false */
+        fldTest.set(a, null);
+        assertEquals(false, a.isMatch(ar));
+        fldLimit2.set(ar, null);
+        assertEquals(false, a.isMatch(ar));
+
+        /** when limits are not specified parameter does not matter */
+        fldLimit1.set(ar, null);
+        assertEquals(true, a.isMatch(ar));
+        fldTest.set(a, 100.0);
+        assertEquals(true, a.isMatch(ar));
+
+        /** one limit is specified */
+        fldLimit1.set(ar, 150.0);
+        assertEquals(false, a.isMatch(ar));
+        fldLimit1.set(ar, 100.0);
+        assertEquals(true, a.isMatch(ar));
+
+        /** another limit is specified */
+        fldLimit1.set(ar, null);
+        fldLimit2.set(ar, 100.0);
+        assertEquals(true, a.isMatch(ar));
+        fldLimit2.set(ar, 50.0);
+        assertEquals(false, a.isMatch(ar));
+
+        /** both limits are specified */
+        fldLimit1.set(ar, 50.0);
+        fldLimit2.set(ar, 150.0);
+        fldTest.set(a, 20.0);
+        assertEquals(false, a.isMatch(ar));
+        fldTest.set(a, 50.0);
+        assertEquals(true, a.isMatch(ar));
+        fldTest.set(a, 100.0);
+        assertEquals(true, a.isMatch(ar));
+        fldTest.set(a, 150.0);
+        assertEquals(true, a.isMatch(ar));
+        fldTest.set(a, 151.0);
+        assertEquals(false, a.isMatch(ar));
+
+        fldTest.set(a, oldTest);
+        fldLimit1.set(ar, oldLimit1);
+        fldLimit2.set(ar, oldLimit2);
+    }
+
+    public static void testId(RealtyBase a, RequirementBase ar, String paramName, String filterName) throws NoSuchFieldException, IllegalAccessException {
+        Field fldParam = a.getClass().getField(paramName);
+        Field fldFilter = ar.getClass().getField(filterName);
+
+        Long oldParam = (Long) fldParam.get(a);
+        Long oldFilter = (Long) fldFilter.get(ar);
+
+        /** when filter is null always return true **/
+        fldFilter.set(ar, null);
+        fldParam.set(a, 10L);
+        assertEquals(true, a.isMatch(ar));
+        fldParam.set(a, 100L);
+        assertEquals(true, a.isMatch(ar));
+        fldParam.set(a, null);
+        assertEquals(true, a.isMatch(ar));
+
+        /**
+         *  when filter is specified
+         *  return true only if param is specified and it is equal to filter
+         */
+        fldFilter.set(ar, 10L);
+        fldParam.set(a, null);
+        assertEquals(false, a.isMatch(ar));
+        fldParam.set(a, 100L);
+        assertEquals(false, a.isMatch(ar));
+        fldParam.set(a, 10L);
+        assertEquals(true, a.isMatch(ar));
+
+        fldParam.set(a, oldParam);
+        fldFilter.set(ar, oldFilter);
+    }
+}
